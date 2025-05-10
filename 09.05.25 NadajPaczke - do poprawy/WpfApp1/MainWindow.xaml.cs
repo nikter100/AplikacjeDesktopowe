e83d6556b6a93710bcd8ec00e6b41a8bfcbc9f8e
Jakub Spendel <jspendel45@gmail.com>
2025-05-10 23:50:28 +0200

    Dodanie projektu Biblioteka oraz dodanie zadania egzaminacyjnego Nadaj Paczkę

---
+++ 09.05.25 NadajPaczke - do poprawy/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sprawdzCeneClick(object sender, RoutedEventArgs e)
+        {
+            if (pocztowka.IsChecked == true)
+            {
+                zdjeciePocztowka.Visibility = Visibility.Visible;
+                zdjecieList.Visibility = Visibility.Collapsed;
+                zdjeciePaczka.Visibility = Visibility.Collapsed;
+
+                cena.Content = "Cena: 1 zł";
+            }
+            else if (list.IsChecked == true)
+            {
+                zdjeciePocztowka.Visibility = Visibility.Collapsed;
+                zdjecieList.Visibility = Visibility.Visible;
+                zdjeciePaczka.Visibility = Visibility.Collapsed;
+
+                cena.Content = "Cena: 1,5 zł";
+            }
+            else if (paczka.IsChecked == true)
+            {
+                zdjeciePocztowka.Visibility = Visibility.Collapsed;
+                zdjecieList.Visibility = Visibility.Collapsed;
+                zdjeciePaczka.Visibility = Visibility.Visible;
+
+                cena.Content = "Cena: 10 zł";
+            }
+        }
+
+        private void zatwierdClick(object sender, RoutedEventArgs e)
+        {
+            if (kodPocztowy.Text.Length > 5 || kodPocztowy.Text.Length < 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+            }
+            else if (int.TryParse(kodPocztowy.Text, out int kod))
+            {
+                MessageBox.Show("Dane przesyłki zostały wprowadzone");
+            }
+            else
+            {
+                MessageBox.Show("Kod podcztowy powinien się składać z samych cyfr");
+            }
+
+        }
+    }
+}
---
+++ Bibltioteka/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private bool sprawdzDane()
+        {
+            if ((string.IsNullOrWhiteSpace(imie.Text)) || string.IsNullOrWhiteSpace(nazwisko.Text) || string.IsNullOrWhiteSpace(adres.Text) || stanowisko.SelectedIndex == 0)
+            {
+                MessageBox.Show("Wszystkie dane powinny zostac wypelnione", "Karta Biblioteczna", MessageBoxButton.OK, MessageBoxImage.Information);
+                noweImie.Text = "-";
+                noweNazwisko.Text = "-";
+                nowyAdres.Text = "-";
+                noweStanowisko.Text = "-";
+
+                czytelnik.Text = "Czytelnik nie zostal jeszcze zapisany";
+
+                return false;
+            }
+            return true;
+        }
+        private void uruchomZapiszDane(object sender, RoutedEventArgs e)
+        {
+            if (sprawdzDane())
+            {
+                noweImie.Text = imie.Text;
+                noweNazwisko.Text = nazwisko.Text;
+                nowyAdres.Text = adres.Text;
+                if (stanowisko.SelectedIndex == 1)
+                {
+                    noweStanowisko.Text = "Nauczyciel";
+                }
+                else if (stanowisko.SelectedIndex == 2)
+                {
+                    noweStanowisko.Text = "Pracownik szkoly";
+                }
+                else if (stanowisko.SelectedIndex == 3)
+                {
+                    noweStanowisko.Text = "Uczen";
+                }
+
+                czytelnik.Text = "Czytelnik zostal zapisany";
+
+                MessageBox.Show("Dane zostaly zapisane", "Karta Biblioteczna", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                imie.Text = string.Empty;
+                nazwisko.Text = string.Empty;
+                adres.Text = string.Empty;
+                stanowisko.SelectedIndex = 0;
+            }
+        }
+    }
+}
