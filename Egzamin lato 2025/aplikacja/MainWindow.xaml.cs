f2798b48dcceac2dd12d05e03a7c38b49a493216
Jakub Spendel <jspendel45@gmail.com>
2025-05-20 20:49:27 +0200

    Stworzenie projektu z egzaminu zawowodego z wyborem albumów

---
+++ Egzamin lato 2025/aplikacja/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace aplikacja
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int i = 0;
+
+        string[] linie = File.ReadAllLines("Data.txt");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click_Pobrania(object sender, RoutedEventArgs e)
+        {
+            string ilePobran = iloscPobran.Content.ToString();
+
+            int pobrania = int.Parse(ilePobran);
+            pobrania++;
+
+            iloscPobran.Content = pobrania;
+        }
+
+        private void Button_Click_Poprzedni(object sender, RoutedEventArgs e)
+        {
+
+            i -= 6;
+
+            if (i < 0)
+            {
+                i = linie.Length - 5;
+            }
+
+            if (i < linie.Length)
+            {
+                artysta.Content = linie[i];
+                album.Content = linie[i + 1];
+                iloscPiosenek.Content = linie[i + 2] + " utworów";
+                rokWydania.Content = linie[i + 3];
+                iloscPobran.Content = linie[i + 4];
+            }
+
+        }
+
+        private void Button_Click_Nastepny(object sender, RoutedEventArgs e)
+        {
+
+            i += 6;
+
+            if (i >= linie.Length)
+            {
+                i = 0;
+            }
+
+            if (i < linie.Length)
+            {
+                
+                artysta.Content = linie[i];
+                album.Content = linie[i + 1];
+                iloscPiosenek.Content = linie[i + 2] + " utworów";
+                rokWydania.Content = linie[i + 3];
+                iloscPobran.Content = linie[i + 4];
+            }
+           
+        }
+    }
+}
---
+++ KamieńPapierNożyce/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int randomChoice()
+        {
+            Random rnd = new Random();
+            int roll = rnd.Next(3);
+
+            return roll;
+        }
+
+        private void AiImage(int rand)
+        {
+            if (rand == 0)
+            {
+                AiChoice.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void check(int choice)
+        {
+            String[] choices = { "Paper", "Kamień", "Nożyce" };
+            int rand = randomChoice();
+            AiImage(rand);
+
+            if (randomChoice() == choice)
+            {
+                MessageBox.Show("Remis! \n\n Twój wybór: " + choices[choice] + " Wybór AI: " + choices[rand]);
+            }
+            else if ((rand == 0 && choice == 1) || (rand == 1 && choice == 2) || (rand == 2 && choice == 0))
+            {
+                MessageBox.Show("Przegrałeś! \n\n Twoj wybór:" + choices[choice] + " Wybór AI: " + choices[rand]);
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+        private void papper_Click(object sender, RoutedEventArgs e)
+        {
+            int choice = 0;
+            MyChoice.Visibility = Visibility.Hidden;
+            MyChoicePapper.Visibility = Visibility.Visible;
+
+            check(choice);
+        }
+
+        private void rock_Click(object sender, RoutedEventArgs e)
+        {
+            int choice = 1;
+            MyChoice.Visibility = Visibility.Hidden;
+            MyChoiceRock.Visibility = Visibility.Visible;
+
+            check(choice);
+        }
+
+        private void scissors_Click(object sender, RoutedEventArgs e)
+        {
+            int choice = 2;
+            MyChoice.Visibility = Visibility.Hidden;
+            MyChoiceScissors.Visibility = Visibility.Visible;
+
+            check(choice);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyChoice.Visibility = Visibility.Visible;
+            MyChoicePapper.Visibility = Visibility.Hidden;
+            MyChoiceRock.Visibility = Visibility.Hidden;
+            MyChoiceScissors.Visibility = Visibility.Hidden;
+        }
+    }
+}
